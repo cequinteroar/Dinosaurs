808a1c4c67dd187efffdca1f37607f83bd5d7a20
Cesar Quintero <cequinteroar@unal.edu.co>
2021-03-17 15:27:02 +0100

    Tiles created, form removed when clicked, Human data gathered from form and compare methods declared but not defined

--- Dino.js
+++ Dino.js
@@ -1,10 +1,12 @@
-function Dino(species, fact, location, age, diet, weight, height){
-    this.species = species;
-    this.fact = fact;
-    this.location = location;
-    this.age = age;
-    this.diet = diet;
-    this.weight = weight;
-    this.height = height;
-    this.picture = ".\images"+species+".png";
+class Dino {
+    constructor(species, fact, location, age, diet, weight, height) {
+        this.species = species;
+        this.fact = fact;
+        this.location = location;
+        this.age = age;
+        this.diet = diet;
+        this.weight = weight;
+        this.height = height;
+        this.picture = "./images/" + species + ".png";
+    }
 }
--- app.js
+++ app.js
@@ -9,36 +9,85 @@ async function createDinos() {
     const dinos = await getDinos();
     let dinosObjects = [];
     dinos.map(dino => {
-        dinosObjects.push(new Dino(dino.species, dino.fact, dino.where, dino.when, dino.diet, dino.weight, dini.height));
+        dinosObjects.push(new Dino(dino.species, dino.fact, dino.where, dino.when, dino.diet, dino.weight, dino.height));
     });
-    return dinosObjects;    
-} 
-    
-
-    // Use IIFE to get human data from form
+    return dinosObjects;
+}
 
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+// Use IIFE to get human data from form
+const getHumanData = (function (user) {
+    const name = document.getElementsByName("name")[0].value;
+    const location = document.getElementsByName("where")[0].value;
+    const height = document.getElementsByName("inches")[0].value;
+    const diet = document.getElementsByName("diet")[0].value;
+    user.name = name;
+    user.location = location;
+    user.height = height;
+    user.diet = diet;
+});
 
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Create Dino Compare Method 1
+Dino.prototype.compareLocation = function () {
+
+}
+
+// Create Dino Compare Method 2
+Dino.prototype.compareHeight = function () {
+
+}
 
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
+// Create Dino Compare Method 3
+Dino.prototype.compareDiet = function () {
 
-    // Remove form from screen
+}
+
+
+// Generate Tiles for each Dino in Array
+function createTileInfo(dino, tileItem) {
+    const title = document.createElement("h3");
+    const img = document.createElement("img");
+    const fact = document.createElement("p");
+    title.textContent = dino.species;
+    img.src = dino.picture;
+    fact.textContent = (dino instanceof Human) ? "":dino.fact;
+    tileItem.appendChild(title);
+    tileItem.appendChild(img);
+    tileItem.appendChild(fact);
+}
+
+function generateTiles(dinos, human){
+    let infosArray = dinos;
+    infosArray.splice(4, 0, human);
+    infosArray.forEach(dino => {
+        const tileItem = document.createElement("div");
+        tileItem.className = "grid-item";
+        createTileInfo(dino, tileItem)
+        document.getElementById("grid").appendChild(tileItem);
+    });
+}
+
+// Add tiles to DOM
+
+// Remove form from screen
+function removeForm() {
+    const form = document.getElementById("dino-compare");
+    form.style.display = "none";
+}
+
+async function compare() {
+    const dinos = await createDinos();
+    // Create Human Object
+    let human = new Human();
+    getHumanData(human);
+    removeForm();
+    generateTiles(dinos, human);
+}
 
 
 // On button click, prepare and display infographic
 
-async function compare(){
-    const dinos = createDinos();
-}
+const compareBtn = document.getElementById("compare");
+compareBtn.addEventListener("click", compare);
